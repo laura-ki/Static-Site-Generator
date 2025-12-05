89984fa36a949d16ac4bbee4efb97c4a70fa6716
laura-ki <laura.vilkaite1@gmail.com>
2025-12-05 15:06:18 +0200

    text to html

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -56,6 +57,12 @@ class TestHTMLNode(unittest.TestCase):
             parent.to_html(),
             '<div class="box"><span>text</span></div>'
         )
-    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     PLAIN_TEXT = "plain"
@@ -19,3 +20,25 @@ class TextNode:
     
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+
+    if text_node.text_type == TextType.PLAIN_TEXT:
+        return LeafNode(None, text_node.text)
+        
+    if text_node.text_type == TextType.BOLD_TEXT:
+        return LeafNode("b", text_node.text)
+        
+    if text_node.text_type == TextType.ITALIC_TEXT:
+        return LeafNode("i", text_node.text)
+    
+    if text_node.text_type == TextType.CODE_TEXT:
+        return LeafNode("code", text_node.text)
+    
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    
+        
