5c223689043b63a6db8ffe3204e11fe29549ea69
laura-ki <laura.vilkaite1@gmail.com>
2026-01-13 13:02:18 +0200

    image and link split

--- src/textnode.py
+++ src/textnode.py
@@ -41,4 +41,3 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
     
-        
